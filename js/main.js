b342196512f50769bfe19990832f1671848a05aa
Iana <sianalucky@gmail.com>
2020-07-28 16:30:09 +0300

    add some js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,94 @@ navToggle.addEventListener('click', function () {
     headerColor.classList.remove('header__wrap--opened');
   }
 });
+
+// Фильтр
+
+let catalogFilterEvents = document.querySelector('.catalog__filter-forEvents')
+
+let catalogBtn = document.querySelector('.catalog__bth')
+let catalogBtnClosed = document.querySelector('.catalog__btn--filter')
+let catalogBtnOpened = document.querySelector('.catalog__btn--cross')
+
+let catalogFilterToggle = document.querySelector('.filter__wrap');
+
+catalogFilterEvents.addEventListener('click', function () {
+  if (catalogFilterToggle.classList.contains('filter__closed')) {
+    catalogFilterToggle.classList.remove('filter__closed');
+    catalogFilterToggle.classList.add('filter__opened');
+  }
+  else {
+    catalogFilterToggle.classList.add('filter__closed');
+    catalogFilterToggle.classList.remove('filter__opened');
+  }
+});
+
+catalogFilterEvents.addEventListener('click', function () {
+  if (catalogFilterToggle.classList.contains('filter__opened')) {
+    catalogBtnClosed.classList.remove('catalog__btn--show');
+    catalogBtnClosed.classList.add('catalog__btn--hidden');
+    catalogBtnOpened.classList.add('catalog__btn--show');
+  }
+  else {
+    catalogBtnOpened.classList.remove('catalog__btn--show');
+    catalogBtnOpened.classList.add('catalog__btn--hidden');
+    catalogBtnClosed.classList.add('catalog__btn--show');
+  }
+})
+
+if (catalogFilterToggle.classList.contains('filter__closed')) {
+  catalogBtnOpened.classList.add('catalog__btn--hidden');
+}
+// else {
+//   catalogBtnClosed.classList.add('catalog__btn--show');
+// }
+
+// let catalogFilterClosed =  document.querySelector('.')
+
+// catalogBtn.addEventListener('click', changeBtnOpen);
+
+// function changeBtnOpen() {
+//   catalogBtn.classList.add('catalog__btn--hidden')
+// }
+// if (changeBtnOpen) {
+//   catalogBtnOpened.classList.add('catalog__btn--hidden');
+// } 
+// catalogBtn.addEventListener('click', changeBtnClose);
+
+// function changeBtnClose() {
+  
+// }
+
+// else {
+//   catalogBtnOpened.classList.add('catalog__btn--show');
+// }
+
+// catalogFilterEvents.addEventListener('click', catalogFilterOpen);
+
+// function catalogFilterOpen() {
+//   catalogFilterOpened.classList.remove('filter__closed');
+// }
+
+// catalogFilterEvents.addEventListener('click', catalogFilterClose);
+
+// function catalogFilterClose{
+//   catalogFilterOpened.classList.add('filter__closed');
+// }
+
+
+// catalogFilterEvents.addEventListener('click', catalogFilterCloded);
+
+// function catalogFilterCloded() {
+//   catalogFilterOpened.classList.add('filter__closed');
+// }
+
+// catalogFilterEvents.addEventListener('click', changeBtn);
+
+// function changeBtn(){
+//   catalogBtn.classList.add('catalog__btn--closed');
+// }
+
+// if (catalogBtn.contains('catalog__btn--closed')) {
+//   catalogBtn.remove('catalog__btn--closed');
+// }
+
