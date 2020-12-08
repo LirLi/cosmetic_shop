ac781872fcd066fe215bb368d87b7738208174ae
Iana <sianalucky@gmail.com>
2020-12-08 15:44:54 +0300

    test

--- js/main.js
+++ js/main.js
@@ -75,7 +75,8 @@ let i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+        to highlight the 
+        button that controls the panel */
         // this.classList.toggle("filter__accordion-btn--opened"); пока не нужно
 
         /* Toggle between hiding and showing the active panel */
